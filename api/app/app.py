1f4f7473f3631129c0693c78ff81e95a026936a0
Dimand-Sok <dimand.sok@gmail.com>
2022-05-29 08:56:19 +0700

    config debugger

--- api/app/app.py
+++ api/app/app.py
@@ -1,3 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+def create_app(config):
+    print(app)
+    return app
--- api/run.py
+++ api/run.py
@@ -1,4 +1,10 @@
-from app import app as current_app
+from app import create_app
+
+from route import auth_route
+
+current_app = create_app(dict())
+
+current_app.register_blueprint(auth_route)
 
 if __name__ == "__main__":
     current_app.run(debug=True, host='0.0.0.0', port=5000)
