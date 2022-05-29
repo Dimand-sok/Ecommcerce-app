7c739f42eb9c9d37f22476d8cf67d892e979678d
Dimand-Sok <dimand.sok@gmail.com>
2022-05-29 08:33:07 +0700

    maping volumn to docker

--- api/route/auth_route.py
+++ api/route/auth_route.py
@@ -3,6 +3,5 @@ from flask import Blueprint, make_response
 auth_route = Blueprint("auth_route",__name__, url_prefix="/api")
 
 @auth_route.route("/")
-
 def register_user():
     return make_response({"message": "Successfully World"})
--- api/run.py
+++ api/run.py
@@ -1,4 +1,4 @@
 from app import app as current_app
 
 if __name__ == "__main__":
-    current_app.run()
+    current_app.run(debug=True, host='0.0.0.0', port=5000)
